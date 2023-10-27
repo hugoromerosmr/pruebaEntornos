153371b6cb862fd4283c774e566df9577d540359
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-10-27 13:12:56 +0200

    solucionado error del merge

--- js/user.js
+++ js/user.js
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 const USER = {
 	name: "Alberto",
 	age: 24,
 	id: "#4432",
 	mail: "adfaf@gmail.com"
-=======
+}
 const USER2 = {
 	name: "pepe",
 	age: 34,
 	id: "#2342",
 	mai: "asdfasdg@gmail.com"
->>>>>>> test
+
 }
